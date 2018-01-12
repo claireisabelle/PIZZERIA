293d74df61c4377e144dea788c632fdc7d1fb38c
claireisabelle <claire.bourdale@gmail.com>
2018-01-12 15:41:00 +0100

    Stylisation header page.php

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ ***********************************************
+ * SET UP THEME
+ ***********************************************
+ */
+
+function lapizzeria_setup(){
+	add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'lapizzeria_setup');
+
 
 /*
  ***********************************************
